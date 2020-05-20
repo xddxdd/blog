51bc07b3b806066d5240b61f887ee69104807635
Lan Tian <xuyh0120@outlook.com>
2020-05-21 01:47:40 +0800

    general: version update and webp caching

--- build.sh
+++ build.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
-RECENT_COMMENTS=$(wget -O- https://lantian.disqus.com/recent_comments_widget.js\?num_items\=10\&hide_mods\=0\&hide_avatars\=1\&excerpt_length\=100)
+# Cache folder
+CACHEDIR=/tmp/hexo-blog
+mkdir -p $CACHEDIR
 
+RECENT_COMMENTS=$(wget -O- https://lantian.disqus.com/recent_comments_widget.js\?num_items\=10\&hide_mods\=0\&hide_avatars\=1\&excerpt_length\=100)
 # Remove delimiters and fixed strings
 RECENT_COMMENTS=$(echo "$RECENT_COMMENTS" | sed "s/Lan Tian @ Blog//g;s/https:\/\/lantian.pub//g")
 RECENT_COMMENTS=$(echo "$RECENT_COMMENTS" | sed "s/ | //g;s/ - //g;s/&nbsp\;&middot\;&nbsp\;//g")
-
 # NodeJS preprocessing
 RECENT_COMMENTS=$(echo "$RECENT_COMMENTS" | sed "s/document.write/process.stdout.write/g")
 RECENT_COMMENTS=$(echo "$RECENT_COMMENTS" | node)
@@ -16,14 +18,15 @@ cp node_modules/@fortawesome/fontawesome-free/webfonts/* themes/lantian/source/a
 
 # Regenerate everything
 rm -rf public .deploy_git
-hexo generate
+node_modules/hexo/bin/hexo generate
 
 # Hexo deploy takes care of git, and baidu_url_submit
-hexo deploy
+node_modules/hexo/bin/hexo deploy
 
 # Compress to gzip, brotli, zstd and webp only for my own site system
 # Useless on other hosts, e.g. GitHub Pages
 echo Preparing parallel jobs...
+echo > parallel_jobs.lst
 for FILE in $(find public -type f \( -name "*.html" -or -name "*.css" -or -name "*.js" -or -name "*.ttf" -or -name "*.atom" -or -name "*.stl" -or -name "*.xml" -or -name "*.svg" -or -name "*.eot" -or -name "*.json" -or -name "*.txt" \)); do
     if [ ! -f $FILE.gz ]; then
         echo gzip -9 -k -f $FILE >> parallel_jobs.lst
@@ -37,14 +40,19 @@ for FILE in $(find public -type f \( -name "*.html" -or -name "*.css" -or -name 
 done
 for FILE in $(find public -type f \( -name "*.gif" -or -name "*.jpg" -or -name "*.png" \)); do
     if [ ! -f $FILE.webp ]; then
-        echo convert -quality 100 $FILE $FILE.webp >> parallel_jobs.lst
+        SHA256=$(sha256sum $FILE | cut -d' ' -f1)
+        if [ -f $CACHEDIR/$SHA256.webp ]; then
+            cp $CACHEDIR/$SHA256.webp $FILE.webp
+        else
+            echo "sh -c \"convert -quality 100 $FILE $FILE.webp && cp $FILE.webp $CACHEDIR/$SHA256.webp\"" >> parallel_jobs.lst
+        fi
     fi
 done
+echo Executing parallel jobs...
 parallel -j$(nproc) < parallel_jobs.lst
-rm parallel_jobs.lst
 
 # Deploy to my site system
 ansible website -m synchronize -a "src=public/ dest=/srv/www/lantian.pub/"
 
 # Index new posts on Algolia
-hexo algolia
+node_modules/hexo/bin/hexo algolia
