20049e03006aa72890346372a019c113b3fa48ee
Lan Tian <xuyh0120@outlook.com>
2020-09-25 01:54:21 +0800

    general: reformat stuff

--- themes/lantian/source/_assets/script.search.js
+++ themes/lantian/source/_assets/script.search.js
@@ -3,47 +3,70 @@ import autocomplete from 'autocomplete.js';
 
 import attempt from './js/attempt';
 
-addLoadEvent(function() {
+addLoadEvent(function () {
     'use strict';
-    attempt('Algolia Search', function() {
+    attempt('Algolia Search', function () {
         'use strict';
-        var client = algoliasearch('***REMOVED***', '***REMOVED***');
+        var client = algoliasearch(
+            '***REMOVED***',
+            '***REMOVED***',
+        );
         var index = client.initIndex('lantian');
 
         function newHitsSource(index, params) {
             return function doSearch(query, cb) {
-              index
-                .search(query, params)
-                .then(function(res) {
-                  cb(res.hits, res);
-                })
-                .catch(function(err) {
-                  console.error(err);
-                  cb([]);
-                });
+                index
+                    .search(query, params)
+                    .then(function (res) {
+                        cb(res.hits, res);
+                    })
+                    .catch(function (err) {
+                        console.error(err);
+                        cb([]);
+                    });
             };
         }
 
-        autocomplete('#search-input', {hint: false}, {
-            source: newHitsSource(index, { hitsPerPage: 5 }),
-            displayKey: 'title',
-            templates:  {
-                suggestion: function(suggestion) {
-                    let dateObj = new Date(suggestion.date);
-                    let dateStr = "";
-                    dateStr += (dateObj.getFullYear() % 100) + '-';
-                    dateStr += (dateObj.getMonth() < 9 ? '0' : '') + (dateObj.getMonth() + 1) + '-';
-                    dateStr += (dateObj.getDate() <= 9 ? '0' : '') + dateObj.getDate();
+        autocomplete(
+            '#search-input',
+            { hint: false },
+            {
+                source: newHitsSource(index, { hitsPerPage: 5 }),
+                displayKey: 'title',
+                templates: {
+                    suggestion: function (suggestion) {
+                        let dateObj = new Date(suggestion.date);
+                        let dateStr = '';
+                        dateStr += (dateObj.getFullYear() % 100) + '-';
+                        dateStr +=
+                            (dateObj.getMonth() < 9 ? '0' : '') +
+                            (dateObj.getMonth() + 1) +
+                            '-';
+                        dateStr +=
+                            (dateObj.getDate() <= 9 ? '0' : '') +
+                            dateObj.getDate();
 
-                    return dateStr + '《<span class="aa-suggestion-title">' + suggestion.title + '</span>》';
+                        return (
+                            dateStr +
+                            '《<span class="aa-suggestion-title">' +
+                            suggestion.title +
+                            '</span>》'
+                        );
+                    },
+                    footer:
+                        '<div class=\'text-right\'><small>Search provided by Algolia</small> <i class="fab fa-lg fa-algolia"></i></div>',
                 },
-                footer: "<div class='text-right'><small>Search provided by Algolia</small> <i class=\"fab fa-lg fa-algolia\"></i></div>"
-            }
-        }).on('autocomplete:selected', function(event, suggestion, dataset, context) {
+            },
+        ).on('autocomplete:selected', function (
+            event,
+            suggestion,
+            dataset,
+            context,
+        ) {
             window.location.href = suggestion.permalink;
         });
 
-        document.getElementById('search-button').onclick = function() {
+        document.getElementById('search-button').onclick = function () {
             document.getElementById('search-bar').classList.toggle('d-none');
         };
     });
--- themes/lantian/webpack.config.js
+++ themes/lantian/webpack.config.js
@@ -1,145 +1,162 @@
 const TerserJSPlugin = require('terser-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-const SassInlineSVG = require('sass-inline-svg')(__dirname, { optimize: true, encodingFormat: 'uri' });
+const SassInlineSVG = require('sass-inline-svg')(__dirname, {
+    optimize: true,
+    encodingFormat: 'uri',
+});
 const path = require('path');
 
 module.exports = {
-  entry: {
-    main: '_assets/script.main.js',
-    search: '_assets/script.search.js',
-    style: '_assets/script.style.js',
-  },
-  output: {
-    filename: 'script.[name].bundle.js',
-    path: 'assets',
-  },
+    entry: {
+        main: '_assets/script.main.js',
+        search: '_assets/script.search.js',
+        style: '_assets/script.style.js',
+    },
+    output: {
+        filename: 'script.[name].bundle.js',
+        path: 'assets',
+    },
 
-  // mode: 'development',
-  mode: 'production',
-  optimization: {
-    minimizer: [
-      new TerserJSPlugin({
-        terserOptions: {
-          parse: {},
-          compress: {
-            arguments: true,
-            booleans_as_integers: true,
-            // drop_console: true,
-            passes: 2,
-          },
-          output: {
-            comments: false,
-          },
-          ie8: true,
-          safari10: true,
-        },
-        extractComments: false,
-      }), 
-      new OptimizeCSSAssetsPlugin({
-        cssProcessorPluginOptions: {
-          preset: [
-            'advanced', 
-            {
-              autoprefixer: { add: true, remove: true, grid: "autoplace" },
-              cssDeclarationSorter: { order: 'smacss' },
-              discardComments: { removeAll: true }
-            }
-          ],
-        },
-      })
-    ],
-  },
-  plugins: [
-    new MiniCssExtractPlugin()
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        // exclude: /(node_modules|bower_components)/,
-        loader: 'babel-loader',
-        options: {
-          presets: [
-            [
-              "@babel/preset-env",
-              {
-                bugfixes: true,
-                useBuiltIns: 'entry',
-                corejs: 3,
-                forceAllTransforms: true,
-                shippedProposals: true,
-              }
-            ]
-          ],
-        }
-      },
-      {
-        test: /\.s[ac]ss$/i,
-        use: [
-          // Extract CSS to file
-          MiniCssExtractPlugin.loader,
-
-          // Translates CSS into CommonJS
-          'css-loader',
-
-          // Translate resource path
-          {
-            loader: 'resolve-url-loader',
-            options: {
-              keepQuery: true,
-            }
-          },
-
-          // Compiles Sass to CSS
-          {
-            loader: 'sass-loader',
-            options: {
-              sourceMap: true,
-              implementation: require('node-sass'),
-              /* https://github.com/haithembelhaj/sass-inline-svg/issues/16 */
-              sassOptions: (loaderContext) => ({
-                functions: {
-                  svg(filePath, done) {
-                    /**
-                     * That loader helper function will resolve webpack aliased imports into absolute strings
-                     * svg("~@images/icon.svg") -> /var/www/app/static/img/icon.svg
-                     *
-                     * Sass import might include webpack alias (like ~@images/)
-                     * These aliased imports always start with '~', so if we see one - we modify it into valid webpack alias
-                     */
-                    const filePathValue = filePath.getValue();
-                    const resultPath = filePathValue.indexOf('~') !== -1 ? filePathValue.substr(1) : filePathValue;
-                    loaderContext.resolve(__dirname, resultPath, (error, result) => {
-                      if (error) {
-                        throw error;
-                      }
-                      // eslint-disable-next-line global-require
-                      const SassString = require('node-sass').types.String;
-                      done(SassInlineSVG(SassString(result)));
-                    });
-                  },
+    // mode: 'development',
+    mode: 'production',
+    optimization: {
+        minimizer: [
+            new TerserJSPlugin({
+                terserOptions: {
+                    parse: {},
+                    compress: {
+                        arguments: true,
+                        booleans_as_integers: true,
+                        // drop_console: true,
+                        passes: 2,
+                    },
+                    output: {
+                        comments: false,
+                    },
+                    ie8: true,
+                    safari10: true,
                 },
-              }),
-            }
-          },
+                extractComments: false,
+            }),
+            new OptimizeCSSAssetsPlugin({
+                cssProcessorPluginOptions: {
+                    preset: [
+                        'advanced',
+                        {
+                            autoprefixer: {
+                                add: true,
+                                remove: true,
+                                grid: 'autoplace',
+                            },
+                            cssDeclarationSorter: { order: 'smacss' },
+                            discardComments: { removeAll: true },
+                        },
+                    ],
+                },
+            }),
         ],
-      },
-      {
-        test: /\.(woff(2)?|ttf|eot|svg)$/i,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: '[name].[ext]',
-              outputPath: 'res',
-            }
-          }
-        ]
-      }
-    ]
-  },
-  stats: {
-    colors: true
-  }
+    },
+    plugins: [new MiniCssExtractPlugin()],
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                // exclude: /(node_modules|bower_components)/,
+                loader: 'babel-loader',
+                options: {
+                    presets: [
+                        [
+                            '@babel/preset-env',
+                            {
+                                bugfixes: true,
+                                useBuiltIns: 'entry',
+                                corejs: 3,
+                                forceAllTransforms: true,
+                                shippedProposals: true,
+                            },
+                        ],
+                    ],
+                },
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    // Extract CSS to file
+                    MiniCssExtractPlugin.loader,
+
+                    // Translates CSS into CommonJS
+                    'css-loader',
+
+                    // Translate resource path
+                    {
+                        loader: 'resolve-url-loader',
+                        options: {
+                            keepQuery: true,
+                        },
+                    },
+
+                    // Compiles Sass to CSS
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            sourceMap: true,
+                            implementation: require('node-sass'),
+                            /* https://github.com/haithembelhaj/sass-inline-svg/issues/16 */
+                            sassOptions: (loaderContext) => ({
+                                functions: {
+                                    svg(filePath, done) {
+                                        /**
+                                         * That loader helper function will resolve webpack aliased imports into absolute strings
+                                         * svg("~@images/icon.svg") -> /var/www/app/static/img/icon.svg
+                                         *
+                                         * Sass import might include webpack alias (like ~@images/)
+                                         * These aliased imports always start with '~', so if we see one - we modify it into valid webpack alias
+                                         */
+                                        const filePathValue = filePath.getValue();
+                                        const resultPath =
+                                            filePathValue.indexOf('~') !== -1
+                                                ? filePathValue.substr(1)
+                                                : filePathValue;
+                                        loaderContext.resolve(
+                                            __dirname,
+                                            resultPath,
+                                            (error, result) => {
+                                                if (error) {
+                                                    throw error;
+                                                }
+                                                // eslint-disable-next-line global-require
+                                                const SassString = require('node-sass')
+                                                    .types.String;
+                                                done(
+                                                    SassInlineSVG(
+                                                        SassString(result),
+                                                    ),
+                                                );
+                                            },
+                                        );
+                                    },
+                                },
+                            }),
+                        },
+                    },
+                ],
+            },
+            {
+                test: /\.(woff(2)?|ttf|eot|svg)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: 'res',
+                        },
+                    },
+                ],
+            },
+        ],
+    },
+    stats: {
+        colors: true,
+    },
 };
