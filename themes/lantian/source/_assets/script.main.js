85a76936c9d5f9d20ad66654bbeca932fb60e674
Lan Tian <xuyh0120@outlook.com>
2020-09-25 01:54:21 +0800

    general: reformat stuff

--- themes/lantian/source/_assets/script.main.js
+++ themes/lantian/source/_assets/script.main.js
@@ -13,17 +13,17 @@ import './js/analytics';
  * Bootstrap Native
  *****************************************/
 
-import initCallback from 'bootstrap.native/src/util/initCallback.js'
+import initCallback from 'bootstrap.native/src/util/initCallback.js';
 // import removeDataAPI from 'bootstrap.native/src/util/removeDataAPI.js'
 
-import componentsInit from 'bootstrap.native/src/util/componentsInit.js'
+import componentsInit from 'bootstrap.native/src/util/componentsInit.js';
 // import {Util} from 'bootstrap.native/src/util/util.js'
 
 // import Alert from 'bootstrap.native/src/components/alert-native.js'
 // import Button from 'bootstrap.native/src/components/button-native.js'
 // import Carousel from 'bootstrap.native/src/components/carousel-native.js'
 // import Collapse from 'bootstrap.native/src/components/collapse-native.js'
-import Dropdown from 'bootstrap.native/src/components/dropdown-native.js'
+import Dropdown from 'bootstrap.native/src/components/dropdown-native.js';
 // import Modal from 'bootstrap.native/src/components/modal-native.js'
 // import Popover from 'bootstrap.native/src/components/popover-native.js'
 // import ScrollSpy from 'bootstrap.native/src/components/scrollspy-native.js'
@@ -35,7 +35,7 @@ import Dropdown from 'bootstrap.native/src/components/dropdown-native.js'
 // componentsInit.Button = [ Button, '[data-toggle="buttons"]' ]
 // componentsInit.Carousel = [ Carousel, '[data-ride="carousel"]' ]
 // componentsInit.Collapse = [ Collapse, '[data-toggle="collapse"]' ]
-componentsInit.Dropdown = [ Dropdown, '[data-toggle="dropdown"]']
+componentsInit.Dropdown = [Dropdown, '[data-toggle="dropdown"]'];
 // componentsInit.Modal = [ Modal, '[data-toggle="modal"]' ]
 // componentsInit.Popover = [ Popover, '[data-toggle="popover"],[data-tip="popover"]' ]
 // componentsInit.ScrollSpy = [ ScrollSpy, '[data-spy="scroll"]' ]
@@ -47,43 +47,45 @@ componentsInit.Dropdown = [ Dropdown, '[data-toggle="dropdown"]']
  * Page Onload Logic
  *****************************************/
 
-addLoadEvent(function() {
+addLoadEvent(function () {
     'use strict';
 
     attempt('Bootstrap.Native', initCallback);
 
-    attempt('Google Analytics', function() {
+    attempt('Google Analytics', function () {
         'use strict';
-        ga('create','UA-37067735-1');
+        ga('create', 'UA-37067735-1');
         ga('send', 'pageview', location.pathname + location.search);
     });
 
-    attempt('Simple Lightbox', function() {
+    attempt('Simple Lightbox', function () {
         'use strict';
-        let lightbox_onclick = function(e) {
-            SimpleLightbox.open({items: [this.getAttribute("src") || this.getAttribute("href")]});
+        let lightbox_onclick = function (e) {
+            SimpleLightbox.open({
+                items: [this.getAttribute('src') || this.getAttribute('href')],
+            });
             try {
                 e.preventDefault();
-            } catch(e) {}
+            } catch (e) {}
             return false;
         };
 
         let posts = document.getElementsByClassName('post-text');
-        for(let i = 0; i < posts.length; i++) {
+        for (let i = 0; i < posts.length; i++) {
             let images = posts[i].getElementsByTagName('img');
-            for(let j = 0; j < images.length; j++) {
+            for (let j = 0; j < images.length; j++) {
                 images[j].onclick = lightbox_onclick;
                 images[j].style.cursor = 'pointer';
             }
         }
 
         let qrcodes = document.getElementsByClassName('qrcode-box');
-        for(let i = 0; i < qrcodes.length; i++) {
+        for (let i = 0; i < qrcodes.length; i++) {
             qrcodes[i].onclick = lightbox_onclick;
         }
     });
 
-    attempt('ElderClock', function() {
+    attempt('ElderClock', function () {
         'use strict';
         elderClock();
     });
