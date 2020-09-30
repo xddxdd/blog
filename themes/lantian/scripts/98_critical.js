8780c672ecfb1b8a305e6277a0fe33236a64b1b0
Lan Tian <xuyh0120@outlook.com>
2020-09-30 21:01:23 +0800

    general: multithreaded critical css

--- themes/lantian/scripts/98_critical.js
+++ themes/lantian/scripts/98_critical.js
@@ -1,24 +1,34 @@
 'use strict';
 
-
+const os = require('os');
 const critical = require('critical');
 const path = require('path');
 const fs = require('hexo-fs');
 const minimatch = require('minimatch');
 
 function getCriticalOptions(html_file) {
-    var perPageCss = true;
+    let perPageCss = true;
 
     if (perPageCss) {
-        var fullyQualifiedFilename = path.join(hexo.public_dir, html_file);
-        var base = path.dirname(fullyQualifiedFilename) + '/';
+        let fullyQualifiedFilename = path.join(hexo.public_dir, html_file);
+        let base = path.dirname(fullyQualifiedFilename) + '/';
 
         return {
             base: base,
             src: fullyQualifiedFilename,
             inline: true,
             minify: true,
-            ignore: ['@font-face']
+            dimensions: [
+                {
+                    height: 640,
+                    width: 360,
+                },
+                {
+                    height: 720,
+                    width: 1280,
+                },
+            ],
+            ignore: ['@font-face'],
         };
     } else {
         return {
@@ -26,73 +36,113 @@ function getCriticalOptions(html_file) {
             src: html_file,
             inline: true,
             minify: true,
-            ignore: ['@font-face']
+            dimensions: [
+                {
+                    height: 640,
+                    width: 360,
+                },
+                {
+                    height: 720,
+                    width: 1280,
+                },
+            ],
+            ignore: ['@font-face'],
         };
     }
 }
 
-function applyCriticalToFile(html_file) {
-    var dest_file = hexo.public_dir + '/' + html_file;
-    var options = getCriticalOptions(html_file);
+function applyCriticalToFile(html_file, thread_id) {
+    let dest_file = hexo.public_dir + '/' + html_file;
+    let options = getCriticalOptions(html_file);
 
     if (!options.inline) {
         hexo.log.error('Critical CSS must be inlined');
         return;
     }
 
-    var criticalPromise = critical.generate(options);
+    let criticalPromise = critical.generate(options);
 
-    return criticalPromise.then(({css, html, unneeded}) => {
-        fs.writeFileSync(dest_file, html);
+    return criticalPromise
+        .then(({ css, html, unneeded }) => {
+            fs.writeFileSync(dest_file, html);
 
-        hexo.log.log('Generated critical CSS for', html_file);
+            hexo.log.log(
+                'Critical CSS: Thread',
+                thread_id,
+                'completed',
+                html_file,
+            );
 
-        return;
-    })
-    .catch(function(error) {
-        hexo.log.error('Unable to inline critical CSS for', html_file);
-        hexo.log.error(error);
-    });
+            return;
+        })
+        .catch(function (error) {
+            hexo.log.error(
+                'Critical CSS: Thread',
+                thread_id,
+                'failed',
+                html_file,
+            );
+            hexo.log.error(error);
+        });
+}
+
+function multithread_split(array, parts) {
+    let result = [];
+    for (let i = parts; i > 0; i--) {
+        result.push(array.splice(0, Math.ceil(array.length / i)));
+    }
+    return result;
+}
+
+function multithread_wait(queues, i, count, resolve) {
+    if (i >= count) {
+        resolve();
+    } else {
+        return queues[i].then(function () {
+            multithread_wait(queues, i + 1, count, resolve);
+        });
+    }
 }
 
 function CriticalCssWorker() {
+    const threads = os.cpus().length * 2;
+
     return fs.exists(hexo.public_dir).then(function (exist) {
         if (!exist) {
-            hexo.log.error('public directory does not exist');
             return;
         }
 
-        var files = fs.listDirSync(hexo.public_dir);
-        files = files.filter(function(file) {
-            return hexo.config.skip_render.reduce(function(kept, filter) {
+        let files = fs.listDirSync(hexo.public_dir);
+        files = files.filter(function (file) {
+            return hexo.config.skip_render.reduce(function (kept, filter) {
                 return kept && !minimatch(file, filter);
             }, true);
         });
-        files = files.filter(function(name) {
+        files = files.filter(function (name) {
             const extname = path.extname(name) || path.basename(name) || name;
-            const extension =  extname[0] === '.' ? extname.slice(1) : extname;
+            const extension = extname[0] === '.' ? extname.slice(1) : extname;
             return extension === 'html' || extension === 'htm';
         });
 
-        return new Promise(function(resolve, reject) {
-            var initialPromiseQueue = Promise.resolve();
+        return new Promise(function (resolve, reject) {
+            let queues = new Array(threads);
+            let file_chunks = multithread_split(files, threads);
+            for (let i = 0; i < threads; i++) {
+                queues[i] = file_chunks[i].reduce(function (queue, html_file) {
+                    return queue.then(function () {
+                        return applyCriticalToFile(html_file, i);
+                    });
+                }, Promise.resolve());
+            }
 
-            files.reduce(function(promiseQueue, html_file) {
-                return promiseQueue.then(function() {
-                    return applyCriticalToFile(html_file);
-                });
-            }, initialPromiseQueue).then(function() {
-                resolve();
-            });
-        }).then(function(resolvedPromises) {
-            hexo.log.log('Finished including all critical css into the html files');
+            return multithread_wait(queues, 0, threads, resolve);
+        }).then(function (resolvedPromises) {
+            hexo.log.log(
+                'Finished including all critical css into the html files',
+            );
             return resolvedPromises;
         });
     });
 }
 
-hexo.extend.filter.register(
-    'before_exit',
-    CriticalCssWorker,
-    10000
-);
+hexo.extend.filter.register('before_exit', CriticalCssWorker, 10000);
