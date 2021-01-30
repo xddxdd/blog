a71a7525e22856811cbcc291be3f152f33854cdd
Lan Tian <xuyh0120@outlook.com>
2021-01-30 22:48:22 +0800

    interactive: add initial test document

---
+++ source/lab/interactive/index.md
@@ -0,0 +1,91 @@
+---
+title: '互动文章测试页'
+date: 1970-01-01 00:00:00
+---
+
+使用下面的支线选项进行测试。
+
+{% interactive_buttons %}
+t1 支线 1
+t2 支线 2
+{% endinteractive_buttons %}
+
+{% interactive t1 %}
+支线 1
+{% interactive_buttons %}
+t11 支线 1-1
+t12 支线 1-2
+{% endinteractive_buttons %}
+{% endinteractive %}
+
+{% interactive t2 %}
+支线 2
+{% interactive_buttons %}
+t21 支线 2-1
+t22 支线 2-2
+{% endinteractive_buttons %}
+{% endinteractive %}
+
+{% interactive t11 %}
+支线 1-1
+{% interactive_buttons %}
+t111 支线 1-1-1
+t112 支线 1-1-2
+{% endinteractive_buttons %}
+{% endinteractive %}
+
+{% interactive t12 %}
+支线 1-2
+{% interactive_buttons %}
+t121 支线 1-2-1
+t122 支线 1-2-2
+{% endinteractive_buttons %}
+{% endinteractive %}
+
+{% interactive t21 %}
+支线 2-1
+{% interactive_buttons %}
+t211 支线 2-1-1
+t212 支线 2-1-2
+{% endinteractive_buttons %}
+{% endinteractive %}
+
+{% interactive t22 %}
+支线 2-2
+{% interactive_buttons %}
+t221 支线 2-2-1
+t222 支线 2-2-2
+{% endinteractive_buttons %}
+{% endinteractive %}
+
+{% interactive t111 %}
+支线 1-1-1
+{% endinteractive %}
+
+{% interactive t112 %}
+支线 1-1-2
+{% endinteractive %}
+
+{% interactive t121 %}
+支线 1-2-1
+{% endinteractive %}
+
+{% interactive t122 %}
+支线 1-2-2
+{% endinteractive %}
+
+{% interactive t211 %}
+支线 2-1-1
+{% endinteractive %}
+
+{% interactive t212 %}
+支线 2-1-2
+{% endinteractive %}
+
+{% interactive t221 %}
+支线 2-2-1
+{% endinteractive %}
+
+{% interactive t222 %}
+支线 2-2-2
+{% endinteractive %}
--- themes/lantian/scripts/00_lantian.js
+++ themes/lantian/scripts/00_lantian.js
@@ -1,30 +1,96 @@
 const execSync = require('child_process').execSync;
 const gitRevision = execSync('git log -1 --format=%h').toString().trim();
 
-hexo.extend.helper.register('remove_trailing_slash', function(s) {
-    if(s[s.length - 1] == '/') {
+hexo.extend.helper.register('remove_trailing_slash', function (s) {
+    if (s[s.length - 1] == '/') {
         return s.substring(0, s.length - 1);
-    } else if(s.substring(s.length - 11) == '/index.html') {
+    } else if (s.substring(s.length - 11) == '/index.html') {
         return s.substring(0, s.length - 11);
     } else {
         return s;
     }
 });
 
-hexo.extend.helper.register('lantian_excerpt', function(input) {
+hexo.extend.helper.register('lantian_excerpt', function (input) {
     var excerpt_length = 400;
-    var stripped = input.replace(/<\/?("[^"]*"|'[^']*'|[^>])*(>|$)/g, "");
+    var stripped = input.replace(/<\/?("[^"]*"|'[^']*'|[^>])*(>|$)/g, '');
     var separators = ['。', '，', '.', ',', '：', ':', ')', '）'];
     var output_until = excerpt_length;
-    for(var i = excerpt_length; i > 0; i--) {
-        if(separators.includes(stripped[i])) {
+    for (var i = excerpt_length; i > 0; i--) {
+        if (separators.includes(stripped[i])) {
             output_until = i + 1;
             break;
         }
     }
-    return stripped.substr(0, output_until) + "...";
+    return stripped.substr(0, output_until) + '...';
 });
 
-hexo.extend.helper.register('lantian_git_rev', function() {
+hexo.extend.helper.register('lantian_git_rev', function () {
     return gitRevision;
 });
+
+function markdown_render(content) {
+    const MdIt = require('markdown-it');
+    const {
+        preset,
+        render,
+        enable_rules,
+        disable_rules,
+        plugins,
+    } = hexo.config.markdown;
+    let parser = new MdIt(preset, render);
+
+    if (enable_rules) {
+        parser.enable(enable_rules);
+    }
+
+    if (disable_rules) {
+        parser.disable(disable_rules);
+    }
+
+    if (plugins) {
+        parser = plugins.reduce((parser, pugs) => {
+            if (pugs instanceof Object && pugs.name) {
+                return parser.use(require(pugs.name), pugs.options);
+            }
+            return parser.use(require(pugs));
+        }, parser);
+    }
+
+    hexo.execFilterSync('markdown-it:renderer', parser, { context: hexo });
+
+    return parser.render(content);
+}
+
+hexo.extend.tag.register(
+    'interactive',
+    function (args, content) {
+        var rendered = markdown_render(content);
+        return `<div id="lt-interactive-content-${args[0]}" class="d-none">${rendered}</div>`;
+    },
+    { ends: true },
+);
+
+hexo.extend.tag.register(
+    'interactive_buttons',
+    function (args, content) {
+        var i = Math.floor(Math.random() * 10000000);
+        var s = `<div class="btn-group btn-group-toggle" data-toggle="buttons" id="lt-interactive-group-${i}" class="lt-interactive">`;
+
+        content.split('\n').forEach((v) => {
+            var splitted = v.split(' ');
+            var tag = splitted[0];
+            var label = splitted.slice(1).join(' ');
+
+            s += `
+<label class="btn btn-outline-primary">
+    <input type="radio" name="lt-interactive-group-${i}" id="lt-interactive-${tag}" class="lt-interactive-option" data-group="${i}" data-tag="${tag}">${label}</input>
+</label>`;
+        });
+
+        s += `</div>`;
+
+        return s;
+    },
+    { ends: true },
+);
