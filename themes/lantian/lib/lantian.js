df728d021082bb2013d0e550ab5eb53581f231dd
Lan Tian <xuyh0120@outlook.com>
2021-03-21 02:09:00 +0800

    general: initial version of gopher site

---
+++ themes/lantian/lib/lantian.js
@@ -0,0 +1,25 @@
+const isFullwidthCodePoint = require('is-fullwidth-code-point');
+
+function slice_width(str, start, length) {
+    var idx_first = 0;
+    while (true) {
+        var w = isFullwidthCodePoint(str.codePointAt(idx_first)) ? 2 : 1;
+        if (start < w) break;
+        idx_first++;
+        start -= w;
+    }
+
+    var idx_last = idx_first;
+    while (true) {
+        var w = isFullwidthCodePoint(str.codePointAt(idx_last)) ? 2 : 1;
+        if (length < w) break;
+        idx_last++;
+        length -= w;
+    }
+    return str.slice(idx_first, idx_last);
+}
+
+module.exports = {
+    EXCERPT_REGEX: /<\/?("[^"]*"|'[^']*'|[^>])*(>|$)/g,
+    slice_width: slice_width,
+};
