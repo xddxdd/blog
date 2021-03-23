a61a14633ba349a770963cec7253f74b46c72373
Lan Tian <xuyh0120@outlook.com>
2021-03-24 02:35:52 +0800

    general: update gopher article generation

--- themes/lantian/lib/lantian.js
+++ themes/lantian/lib/lantian.js
@@ -6,6 +6,7 @@ function slice_width(str, start, length) {
         var w = isFullwidthCodePoint(str.codePointAt(idx_first)) ? 2 : 1;
         if (start < w) break;
         idx_first++;
+        if (idx_first >= str.length) break;
         start -= w;
     }
 
@@ -14,6 +15,7 @@ function slice_width(str, start, length) {
         var w = isFullwidthCodePoint(str.codePointAt(idx_last)) ? 2 : 1;
         if (length < w) break;
         idx_last++;
+        if (idx_last >= str.length) break;
         length -= w;
     }
     return str.slice(idx_first, idx_last);
