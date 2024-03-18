2a0377f31fdc921f79f380b9d8aeb160f23e5a59
Lan Tian <xuyh0120@outlook.com>
2024-03-18 00:28:29 -0700

    Add cssnano

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -4,5 +4,19 @@ module.exports = {
     require('postcss-preset-env'),
     require('postcss-inline-svg'),
     require('postcss-sorting'),
+    require('cssnano')({
+      preset: [
+        'advanced',
+        {
+          autoprefixer: {
+            add: true,
+            remove: true,
+            grid: 'autoplace',
+          },
+          cssDeclarationSorter: { order: 'smacss' },
+          discardComments: { removeAll: true },
+        },
+      ],
+    }),
   ],
 }
