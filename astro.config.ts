6c11ed3c1dd0a7ecaf34719707ef29daa79ce441
Lan Tian <xuyh0120@outlook.com>
2024-06-06 22:42:28 -0700

    general: set format back to "preserve" since Astro.js bug is fixed

--- astro.config.ts
+++ astro.config.ts
@@ -98,8 +98,7 @@ export default defineConfig({
   ],
   markdown: markdownPluginOptions,
   build: {
-    // Using "preserve" format will break language switch links
-    format: 'directory',
+    format: 'preserve',
     assets: 'assets',
   },
   // I use instant.page instead
