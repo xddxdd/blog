d2e73a3bd611cd73ce39e0f9a818c67c7f5c517b
Lan Tian <xuyh0120@outlook.com>
2025-06-15 01:08:59 -0700

    general: generate JXL image

--- _scripts/webp.sh
+++ _scripts/webp.sh
@@ -11,22 +11,16 @@ echo >.parallel_jobs.lst
 
 find dist -type f \( -name "*.gif" -or -name "*.jpg" -or -name "*.png" \) |
     while IFS= read -r FILE; do
-        if [ ! -f "$FILE.webp" ]; then
-            SHA256=$(sha256sum "$FILE" | cut -d' ' -f1)
-            if [ -f "$CACHEDIR/$SHA256.webp" ]; then
-                cp "$CACHEDIR/$SHA256.webp" "$FILE.webp"
-            else
-                echo "convert -quality 100 $FILE $FILE.webp && cp $FILE.webp $CACHEDIR/$SHA256.webp" >>.parallel_jobs.lst
+        for FORMAT in webp avif jxl; do
+            if [ ! -f "$FILE.$FORMAT" ]; then
+                SHA256=$(sha256sum "$FILE" | cut -d' ' -f1)
+                if [ -f "$CACHEDIR/$SHA256.$FORMAT" ]; then
+                    cp "$CACHEDIR/$SHA256.$FORMAT" "$FILE.$FORMAT"
+                else
+                    echo "convert -quality 100 $FILE $FILE.$FORMAT && cp $FILE.$FORMAT $CACHEDIR/$SHA256.$FORMAT" >>.parallel_jobs.lst
+                fi
             fi
-        fi
-        if [ ! -f "$FILE.avif" ]; then
-            SHA256=$(sha256sum "$FILE" | cut -d' ' -f1)
-            if [ -f "$CACHEDIR/$SHA256.avif" ]; then
-                cp "$CACHEDIR/$SHA256.avif" "$FILE.avif"
-            else
-                echo "convert -quality 100 $FILE $FILE.avif && cp $FILE.avif $CACHEDIR/$SHA256.avif" >>.parallel_jobs.lst
-            fi
-        fi
+        done
     done
 
 echo Executing parallel jobs...
