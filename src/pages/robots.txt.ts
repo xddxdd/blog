bf63266df8b4a788e474637b993b421898a47426
Lan Tian <xuyh0120@outlook.com>
2025-08-06 20:44:05 -0700

    general: use AI robots.txt

--- src/pages/robots.txt.ts
+++ src/pages/robots.txt.ts
@@ -1,9 +1,15 @@
-import type { APIContext, APIRoute } from 'astro'
+import type { APIContext } from 'astro'
 
-export const GET: APIRoute = (context: APIContext) => {
+export async function GET(context: APIContext) {
+  const aiRobotsTxt = await fetch(
+    'https://github.com/ai-robots-txt/ai.robots.txt/raw/refs/heads/main/robots.txt'
+  )
+  if (aiRobotsTxt.status != 200) {
+    throw new Error('Cannot download AI Robots.txt')
+  }
+
   const robotsTxt = `
-User-agent: *
-Allow: /
+${await aiRobotsTxt.text()}
 
 Sitemap: ${new URL('sitemap.xml', context.site!.origin).href}
 `.trim()
